7cc65b0daafc167561e851cc1c6ef2ddb8af635c
Erik Corral <erik.secogem@outlook.com>
2025-08-25 21:48:58 -0600

    Mostrar prductos

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class, 'index'])->name('home') ;
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+
